358f6c0bebea35e8810c3c73ab35bd9fcd1f0dca
James Ashdown <james.t.ashdown@gmail.com>
2019-02-21 14:54:51 -0700

    install-ubuntu-18.04-LTS: initial

---
+++ install-ubuntu-18.04-LTS.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "${EUID}" -ne 0 ]; then
+	echo "This script must be run as root"
+	exit
+fi
+
+apt update
+snap refresh
+
+apt install -y default-jre
+apt install -y flac
+apt install -y git
+apt install -y make
+snap install --classic android-studio
+snap install --classic atom
+snap install audacity
+snap install ffmpeg
+snap install gimp
+snap install libreoffice
+snap install obs-studio
+snap install retroarch
