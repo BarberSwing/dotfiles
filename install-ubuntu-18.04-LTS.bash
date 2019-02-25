883af9476bbd3d0b861231bc99af77566b9fa16e
James Ashdown <james.t.ashdown@gmail.com>
2019-02-25 12:35:33 -0700

    install-ubuntu-18.04-LTS: use PPAs for up-to-date packages

--- install-ubuntu-18.04-LTS.bash
+++ install-ubuntu-18.04-LTS.bash
@@ -5,30 +5,55 @@ if [ "${EUID}" -ne 0 ]; then
 	exit
 fi
 
-apt update
-apt upgrade -y
+main() {
 
-apt_list=()
-apt_list+=("audacity")
-apt_list+=("clang")
-apt_list+=("default-jdk")
-apt_list+=("default-jre")
-apt_list+=("ffmpeg")
-apt_list+=("flac")
-apt_list+=("gcc")
-apt_list+=("gimp")
-apt_list+=("git")
-apt_list+=("libreoffice")
-apt_list+=("make")
-#apt_list+=("obs-studio")
-apt_list+=("qemu-kvm")
-apt_list+=("quodlibet")
-apt_list+=("retroarch")
-apt_list+=("transmission")
-apt_list+=("vlc")
+	add_ppa ubuntuhandbook1/audacity # audacity
+	#add_ppa obsproject/obs-studio   # obs-studio
+	add_ppa lazka/ppa                # quodlibet
+	add_ppa libretro/stable          # retroarch
 
-apt install -y "${apt_list[@]}"
+	apt update
+	apt upgrade -y
 
+	apt_list=()
+	apt_list+=("atom")
+	apt_list+=("audacity")
+	apt_list+=("clang")
+	apt_list+=("default-jdk")
+	apt_list+=("default-jre")
+	apt_list+=("ffmpeg")
+	apt_list+=("flac")
+	apt_list+=("gcc")
+	apt_list+=("gimp")
+	apt_list+=("git")
+	apt_list+=("libreoffice")
+	apt_list+=("make")
+	apt_list+=("nautilus-dropbox")
+	#apt_list+=("obs-studio")
+	apt_list+=("qemu-kvm")
+	apt_list+=("quodlibet")
+	apt_list+=("retroarch")
+	apt_list+=("transmission")
 
-snap refresh
-snap install --classic android-studio
+	apt install -y "${apt_list[@]}"
+
+	apt autoremove
+
+	snap refresh
+	snap install --classic android-studio
+	snap install vlc
+}
+
+add_ppa() {
+	for ppa in "${@}"; do
+		grep -hr "^deb.*${ppa}" "/etc/apt/sources.list.d/" > /dev/null 2>&1
+		if [ "${?}" -ne "0" ]; then
+			echo "adding ppa:${ppa}"
+			add-apt-repository -y "ppa:${ppa}"
+		else
+			echo "ppa:${ppa} already exists"
+		fi
+	done
+}
+
+main "${@}"
