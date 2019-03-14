c31126672e520af370133b9fb562869b523ba44d
James Ashdown <james.t.ashdown@gmail.com>
2019-03-14 13:01:38 -0600

    install-lubuntu-18.04-LTS: remove comments, extra packages

--- install-lubuntu-18.04-LTS.bash
+++ install-lubuntu-18.04-LTS.bash
@@ -6,48 +6,40 @@ if [ "${EUID}" -ne 0 ]; then
 fi
 
 main() {
-
 	add_ppa ubuntuhandbook1/audacity # audacity
 	add_ppa phoerious/keepassxc      # keepassxc
-	#add_ppa obsproject/obs-studio   # obs-studio
 	add_ppa lazka/ppa                # quodlibet
 	add_ppa libretro/stable          # retroarch
 
 	apt update
 	apt upgrade -y
 
-	apt_list=()
-	apt_list+=("arandr")
-	#apt_list+=("atom")
-	apt_list+=("audacity")
-	apt_list+=("clang")
-	apt_list+=("default-jdk")
-	apt_list+=("default-jre")
-	#apt_list+=("ffmpeg")
-	#apt_list+=("flac")
-	apt_list+=("gcc")
-	#apt_list+=("gcc-multilib")
-	apt_list+=("gimp")
-	apt_list+=("git")
-	apt_list+=("keepassxc")
-	apt_list+=("libreoffice")
-	#apt_list+=("make")
-	#apt_list+=("nautilus-dropbox")
-	#apt_list+=("obs-studio")
-	apt_list+=("redshift-gtk")
-	#apt_list+=("qemu-kvm")
-	apt_list+=("quodlibet")
-	apt_list+=("retroarch")
-	apt_list+=("rofi")
-	apt_list+=("steam")
-	apt_list+=("steam-devices")
-	apt_list+=("transmission")
-	apt_list+=("valgrind")
-	apt_list+=("xbacklight")
+	packages=()
+	packages+=("arandr")
+	packages+=("audacity")
+	packages+=("clang")
+	packages+=("default-jdk")
+	packages+=("default-jre")
+	packages+=("gcc")
+	packages+=("gimp")
+	packages+=("git")
+	packages+=("keepassxc")
+	packages+=("libreoffice")
+	packages+=("numix-gtk-theme")
+	packages+=("numix-icon-theme")
+	packages+=("quodlibet")
+	packages+=("redshift-gtk")
+	packages+=("retroarch")
+	packages+=("rofi")
+	packages+=("steam")
+	packages+=("steam-devices")
+	packages+=("transmission")
+	packages+=("valgrind")
+	packages+=("xbacklight")
 
-	apt install -y "${apt_list[@]}"
+	apt install -y "${packages[@]}"
 
-	apt autoremove
+	apt autoremove -y
 }
 
 add_ppa() {
